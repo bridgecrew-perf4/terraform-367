a12c01139e437dc4523ab8119e7e5bcf0a973319
Akseli Nurmio <akseli@nurmio.fi>
2020-12-08 22:21:34 +0200

    Add DNS zone settings

--- dns/zones.tf
+++ dns/zones.tf
@@ -1,7 +1,41 @@
+# akselinurmio.fi
 resource "cloudflare_zone" "akselinurmio_fi" {
   zone = "akselinurmio.fi"
 }
+resource "cloudflare_zone_dnssec" "akselinurmio_fi" {
+  zone_id = cloudflare_zone.akselinurmio_fi.id
+}
+resource "cloudflare_zone_settings_override" "akselinurmio_fi" {
+  zone_id = cloudflare_zone.akselinurmio_fi.id
+  settings {
+    always_use_https = "on"
+    brotli           = "on"
+    ssl              = "strict"
+    security_header {
+      enabled            = true
+      preload            = true
+      include_subdomains = true
+    }
+  }
+}
 
+# nurmio.fi
 resource "cloudflare_zone" "nurmio_fi" {
   zone = "nurmio.fi"
+}
+resource "cloudflare_zone_dnssec" "nurmio_fi" {
+  zone_id = cloudflare_zone.nurmio_fi.id
+}
+resource "cloudflare_zone_settings_override" "nurmio_fi" {
+  zone_id = cloudflare_zone.nurmio_fi.id
+  settings {
+    always_use_https = "on"
+    brotli           = "on"
+    ssl              = "strict"
+    security_header {
+      enabled            = true
+      preload            = true
+      include_subdomains = true
+    }
+  }
 }
